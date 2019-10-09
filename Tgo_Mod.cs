7f387ad857ba07a488a14c96fe30694af9d70b98
Tsohg <asaneoil@gmail.com>
2019-10-08 20:38:00 -0400

    Data Collection

--- Tgo_Mod.cs
+++ Tgo_Mod.cs
@@ -9,9 +9,19 @@ using Terraria;
 
 namespace Tgo_Mod
 {
+    public enum TGOCType //TGO command types
+    {
+        mute,
+        kick,
+        ban
+    }
     [ApiVersion(2, 1)]
     public class Tgo_Mod : TerrariaPlugin
     {
+        /// <summary>
+        /// ToDo: Ship collected data to a database.
+        /// </summary>
+        /// <param name="game"></param>
         public Tgo_Mod(Main game) : base(game)
         {
         }
@@ -34,28 +44,31 @@ namespace Tgo_Mod
         /// It is a toggle. Muting same player twice unmutes.
         /// </summary>
         /// <param name="playerName"></param>
-        public void TgoMute(string playerName)
+        public void TgoMute(string playerName, CommandArgs args)
         {
             TSPlayer tplr = GetTSPlayerByName(playerName);
             tplr.mute = !tplr.mute;
 
             //Player has been muted/unmuted so we collect data...
+            Tgo_Mod_Data data = new Tgo_Mod_Data(args.Player.IP, args.Player.Name, tplr.IP, tplr.Name, TGOCType.mute, DateTime.Now);
         }
 
-        public void TgoKick(string playerName, string reason)
+        public void TgoKick(string playerName, string reason, CommandArgs args)
         {
             TSPlayer tplr = GetTSPlayerByName(playerName);
             TShock.Utils.Kick(tplr, reason);
 
             //Player has been kicked so we collect our data...
+            Tgo_Mod_Data data = new Tgo_Mod_Data(args.Player.IP, args.Player.Name, tplr.IP, tplr.Name, TGOCType.kick, DateTime.Now);
         }
 
-        public void TgoBan(string playerName, string reason)
+        public void TgoBan(string playerName, string reason, CommandArgs args)
         {
             TSPlayer tplr = GetTSPlayerByName(playerName);
             TShock.Utils.Ban(tplr, reason);
 
             //Player has been banned so we collect our data...
+            Tgo_Mod_Data data = new Tgo_Mod_Data(args.Player.IP, args.Player.Name, tplr.IP, tplr.Name, TGOCType.ban, DateTime.Now);
         }
 
         private TSPlayer GetTSPlayerByName(string name)
---
+++ Tgo_Mod_Data.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tgo_Mod
+{
+    class Tgo_Mod_Data
+    {
+        string uIP { get; } //IP of command user
+        string uName { get; } //Name of the command user
+        string tIP { get; } //IP of the Target of the command
+        string tName { get; } //Name of the target of the command
+        TGOCType type { get; } //Type of command used
+        DateTime timeStamp { get; } //Time of command execution
+
+        public Tgo_Mod_Data(string uIP, string uName, string tIP, string tName, TGOCType type, DateTime timeStamp)
+        {
+            this.uIP = uIP;
+            this.uName = uName;
+            this.tIP = tIP;
+            this.tName = tName;
+            this.type = type;
+            this.timeStamp = timeStamp;
+        }
+
+        /// <summary>
+        /// Eliminates white spaces of the ToString() method.
+        /// </summary>
+        /// <returns></returns>
+        public string ToCSV()
+        {
+            return ToString().Replace(" ", "");
+        }
+
+        /// <summary>
+        /// Returns in csv format.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "" + uIP + ", " + uName + ", " + tIP + ", " + tName + ", " + type.ToString() + ", " + timeStamp.ToString();
+        }
+    }
+}
